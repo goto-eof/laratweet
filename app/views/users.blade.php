986b93e24e8eef8b34b0f18a24800bc48f3c49c9
jack-vancee <andrei.dodu@hotmail.it>
2014-08-12 01:20:27 +0200

    New changelist

--- app/views/users.blade.php
+++ app/views/users.blade.php
@@ -6,10 +6,10 @@
     <div class="profile">
         <div class="col-xs-6 col-md-4">
             <div><a href="{{ url('user/profile/'.$user->username) }}"><img class="img-circle" src="{{ asset('img/no_photo_128.png') }}" style="width: 48px;height:48px;"></a></div>
-            <div class="username"><a href="{{ url('user/profile/'.$user->username) }}">{{ '@'.$user->username }}</a></div>
+            <div class="username"><a href="{{ url('user/profile/'.$user->username) }}">{{{ '@'.$user->username }}}</a></div>
         </div>
         <div class="col-xs-12 col-md-8">
-            {{ $user->bio }}
+            {{{ empty($user->bio) ? '90% of your problems can be solved by marketing. Solving the other 10% just requires good procrastination skills.' : $user->bio }}}
         </div>
     </div>
     @endforeach
