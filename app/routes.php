bbe6d5ece1c2b5b1d73df96253680ae64af4eb8a
jack-vancee <andrei.dodu@hotmail.it>
2014-08-11 20:31:12 +0200

    New changelist

--- app/routes.php
+++ app/routes.php
@@ -2,12 +2,17 @@
 
 Route::controller('user', 'UserController');
 Route::controller('tweet', 'TweetController');
-Route::get('/', function () {return View::make('index', array('title' => 'Home page'));});
+Route::get('/', function () {
+    return View::make('index', array('title' => 'Home page'));
+});
+
 
 // http://localhost/json/ciao -> ritorna ["ciao", "mondo"]
+/*
 Route::get('json/{data}', function ($data) {
     return Response::json(array($data, 'mondo'));
 });
+*/
 
 // http://localhost/ -> ritorna Ciao Mondo!
 /*Route::get('/', function () {
