2e4d46c5a538736047e19c41c0cc726b63f79fa6
jack-vancee <andrei.dodu@hotmail.it>
2014-08-11 23:32:02 +0200

    New changelist

--- app/controllers/TweetController.php
+++ app/controllers/TweetController.php
@@ -8,18 +8,17 @@ class TweetController extends \BaseController
     {
         $validator = Validator::make(Input::all(), array('tweet' => 'required|min:1|max:140'));
         if ($validator->fails())
-            Redirect::to('user/profile')->withInput()->with('errorMessage', $validator->messages()->first());
-        //$reflector = new ReflectionClass("Tweet");$fn = $reflector->getFileName();dd($fn);
+            return Redirect::to('user/profile/' . Auth::user()->username)->withInput()->with('errorMessage', $validator->messages()->first());
+
         $tweet = new Tweet;
         $tweet->user_id = Auth::id();
         $tweet->text = Input::get('tweet');
         $tweet->status = 'published';
 
         if (!$tweet->save())
-            Redirect::to('user/profile')->withInput()->with('errorMessage', 'Internal error.');
+            return Redirect::to('user/profile/' . Auth::user()->username)->withInput()->with('errorMessage', 'Internal error.');
 
-        $tweets = User::find(Auth::user()->id)->tweets;
-        return Redirect::to('user/profile')->with(array('errorMessage' => 'Published!'))->with('tweets', $tweets);
+        return Redirect::to('user/profile/' . Auth::user()->username)->with('errorMessage', 'done!');
     }
 
 
