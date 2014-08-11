f0976a91a574699318c873fe588c467ec133a553
jack-vancee <andrei.dodu@hotmail.it>
2014-08-12 01:20:33 +0200

    New changelist

--- app/views/timeline.blade.php
+++ app/views/timeline.blade.php
@@ -3,24 +3,17 @@
 @section('body')
 <div class="inner cover">
     @foreach($tweets as $tweet)
-    <div style="min-height: 100px;" class="tweet">
+    <div class="tweet">
         <div class="col-xs-6 col-md-4">
             <div>
-                <a href="{{ url('user/profile/'.$tweet->username) }}">
-                    <img class="img-circle"
-                         src="{{ asset('img/no_photo_128.png') }}"
-                         style="width: 48px;height:48px;">
-                </a>
-            </div>
+                <a href="{{ url('user/profile/'.$tweet->username) }}"><img class="img-circle" src="{{ asset('img/no_photo_128.png') }}"></a></div>
             <div class="username">
-                <a href="{{ url('user/profile/'.$tweet->username) }}">{{ '@'.$tweet->username }}</a>
+                <a href="{{ url('user/profile/'.$tweet->username) }}">{{{ '@'.$tweet->username }}}</a>
             </div>
         </div>
         <div class="col-xs-12 col-md-8" style="text-align: left;">
-            {{ $tweet->text }}
-            <div class="text-align-right">{{
-                \Carbon\Carbon::createFromTimeStamp(strtotime($tweet->created_at))->diffForHumans() }}
-            </div>
+            {{{ $tweet->text }}}
+            <div class="text-align-right">{{ \Carbon\Carbon::createFromTimeStamp(strtotime($tweet->created_at))->diffForHumans() }}</div>
         </div>
     </div>
     @endforeach
