73c5cc73ad9fa5f5b7a5db2a3bd5543ca1a30ea0
jack-vancee <andrei.dodu@hotmail.it>
2014-08-12 02:32:24 +0200

    New changelist

--- app/views/timeline.blade.php
+++ app/views/timeline.blade.php
@@ -5,13 +5,10 @@
     @foreach($tweets as $tweet)
     <div class="tweet">
         <div class="col-xs-6 col-md-4">
-            <div>
-                <a href="{{ url('user/profile/'.$tweet->username) }}"><img class="img-circle" src="{{ asset('img/no_photo_128.png') }}"></a></div>
-            <div class="username">
-                <a href="{{ url('user/profile/'.$tweet->username) }}">{{{ '@'.$tweet->username }}}</a>
-            </div>
+            <div><a href="{{ url('user/profile/'.$tweet->username) }}"><img class="img-circle" src="{{ asset('img/no_photo_128.png') }}"></a></div>
+            <div class="username"><a href="{{ url('user/profile/'.$tweet->username) }}">{{{ '@'.$tweet->username }}}</a></div>
         </div>
-        <div class="col-xs-12 col-md-8" style="text-align: left;">
+        <div class="col-xs-12 col-md-8">
             {{{ $tweet->text }}}
             <div class="text-align-right">{{ \Carbon\Carbon::createFromTimeStamp(strtotime($tweet->created_at))->diffForHumans() }}</div>
         </div>
